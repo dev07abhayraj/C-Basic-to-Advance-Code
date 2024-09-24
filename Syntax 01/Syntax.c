d7893c4cb4ad30deeaece8509c5e7923cedf6121
Abhayraj Patel <dev07abhayrajpatel@gmail.com>
2024-09-25 05:18:49 +0530

    Day 1 basic scenario of c

---
+++ Syntax 01/Syntax.c
@@ -0,0 +1,63 @@
+// Author : Abhayraj Patel
+// Date 25 September 2024
+
+// Basic Syntax of C 
+
+#include <stdio.h>
+
+int main(){ 
+    printf("Hello World\n");
+    printf("Abhayraj\n");
+    return 0;
+}
+
+// Output : 
+// Hello World
+// Hello World
+
+/* This is a basic code of C language to print or display any name like Hello World or Abhayraj
+
+
+------------------------------------------ INFO / MANUAL / OVERVIEW ----------------------------------------
+
+
+[ #include <stdio.h> ]
+---- This is a Header File or also known as the preprocessor file (Help to write and execute basic structure of code and mendatory to mentions in every code)
+
+[ int main() ]
+---- This is a main function where all the code and statement are executed within it ( by writing int main() )
+
+{
+}
+
+---- These curly braces are used to define the block of code , every statement is written inside it
+
+printf()
+---- It is a built in function by C Language that help to give output or display anything on output as we want.
+
+;
+---- This semicolon indicates the end of line or statement. It is important to use semicolon after each line completion
+
+\n
+---- it is escape seaquence character that is used to add a new line like if we not use \n then the output of above code will be Hello WorldAbhayraj but after adding \n it becomes like this
+Hello World
+Abhayraj
+
+return
+---- it is used to display return value if something a function returns then write it if not then return 0. it is also important to mention
+
+"" 
+---- this double quote is used to print the output or string as it inside this double quote 
+
+// 
+---- this is a single line comment 
+
+
+/* 
+----- this is a 
+
+multi 
+
+line comment
+
+*/
