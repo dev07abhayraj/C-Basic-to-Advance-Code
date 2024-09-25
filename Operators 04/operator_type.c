2fc37a4cff6e1363f40e2689d817eb0138503361
Abhayraj Patel <dev07abhayrajpatel@gmail.com>
2024-09-25 07:13:38 +0530

    done

--- Operators 04/operator_type.c
+++ Operators 04/operator_type.c
@@ -5,10 +5,10 @@ There are many types of Operators in C language :
 1. Arthmetic Operator
 2. Assignment operator
 3. Logical operator
-4. Membership operator
+4. Increment & Decrement Operator
 5. Bitwise Operator
 6. Ternary Operator (Conditional Operator)
 7. Relational Operator
-8. Increment & Decrement Operator
+8. Special Operator (*, &,  comma , sizeof() , type() , . , ->)
 
 */
